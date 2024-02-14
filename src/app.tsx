6e1645db67da59cffa3f61ac5046b82485ec2ab7
white-satellite <masayukiuminin8@gmail.com>
2024-02-14 19:14:05 +0900

    日付を表示

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,9 @@
+const date = new Date();
+
 export default function App() {
     return (
       <h1 className="text-3xl font-bold underline">
-        Hello world!
+        {date.getFullYear()}/{date.getMonth()}/{date.getDay()}
       </h1>
     )
   }
