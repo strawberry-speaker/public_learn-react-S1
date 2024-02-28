48ca007e58dfe577587ce05c3304529b1a255593
white-satellite <masayukiuminin8@gmail.com>
2024-02-28 19:55:32 +0900

    【002】チェックボックス表示

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,56 @@
+import './index.css';
+
 export default function App() {
     return (
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <div>
+
+
+        <h1 className="text-3xl font-bold underline">
+          
+        </h1>
+        <h3 className="font-bold">
+          チェックボックス
+        </h3>
+        
+
+        <div className="inline-flex items-center">
+          <label className="relative flex items-center p-3 rounded-full cursor-pointer" htmlFor="ripple-on"
+            data-ripple-dark="true">
+            <input id="ripple-on" type="checkbox"
+              className="before:content[''] peer relative h-5 w-5 cursor-pointer appearance-none rounded-md border border-blue-gray-200 transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:h-12 before:w-12 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity checked:border-gray-900 checked:bg-gray-900 checked:before:bg-gray-900 hover:before:opacity-10" />
+            <span
+              className="absolute text-white transition-opacity opacity-0 pointer-events-none top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 peer-checked:opacity-100">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-3.5 w-3.5" viewBox="0 0 20 20" fill="currentColor"
+                stroke="currentColor" stroke-width="1">
+                <path fill-rule="evenodd"
+                  d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                  clip-rule="evenodd"></path>
+              </svg>
+            </span>
+          </label>
+          <label className="mt-px font-light text-gray-700 cursor-pointer select-none" htmlFor="ripple-on">
+            Ripple Effect On
+          </label>
+        </div>
+        <div className="inline-flex items-center">
+          <label className="relative flex items-center p-3 rounded-full cursor-pointer" htmlFor="ripple-off">
+            <input id="ripple-off" type="checkbox"
+              className="before:content[''] peer relative h-5 w-5 cursor-pointer appearance-none rounded-md border border-blue-gray-200 transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:h-12 before:w-12 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity checked:border-gray-900 checked:bg-gray-900 checked:before:bg-gray-900 hover:before:opacity-10" />
+            <span
+              className="absolute text-white transition-opacity opacity-0 pointer-events-none top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 peer-checked:opacity-100">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-3.5 w-3.5" viewBox="0 0 20 20" fill="currentColor"
+                stroke="currentColor" stroke-width="1">
+                <path fill-rule="evenodd"
+                  d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                  clip-rule="evenodd"></path>
+              </svg>
+            </span>
+          </label>
+          <label className="mt-px font-light text-gray-700 cursor-pointer select-none" htmlFor="ripple-off">
+            Ripple Effect Off
+          </label>
+        </div>
+
+      </div>
     )
   }
